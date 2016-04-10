17bd36aa8a125088925a2c94ef4ba56bc4b36071
Sisu Xi <xisisu@gmail.com>
2016-04-09 23:43:14 -0400

    best time to buy and sell stock

--- BestTimeToBuyAndSellStockII.cpp
+++ BestTimeToBuyAndSellStockII.cpp
@@ -4,8 +4,9 @@
 
 /*
  * Say you have an array for which the ith element is the price of a given stock on day i.
- * If you were only permitted to complete at most one transaction (ie, buy one and sell one share of the stock),
- * design an algorithm to find the maximum profit.
+ * Design an algorithm to find the maximum profit. You may complete as many transactions as you like (ie, buy one and
+ * sell one share of the stock multiple times). However, you may not engage in multiple transactions at the same time
+ * (ie, you must sell the stock before you buy again).
  */
 
 #include <iostream>
@@ -15,21 +16,24 @@
 class Solution {
 public:
     int maxProfit(std::vector<int> const& prices) { // 8 ms
+        // greedy algorithm, reset minSoFar when buy/sell
         int minSoFar = INT32_MAX;
         int profit = 0;
         for (int i = 0; i < prices.size(); ++i) {
             minSoFar = std::min(minSoFar, prices[i]);
-            profit   = std::max(profit, prices[i] - minSoFar);
+            if (prices[i] > minSoFar) {
+                profit += prices[i] - minSoFar;
+                minSoFar = prices[i]; // reset minSoFar
+            }
         }
         return profit;
     }
 };
 
-
 int main() {
     Solution sol;
     std::vector<std::vector<int>> tests = {
-            {}
+              {}
             , {1}
             , {1,2,3,4,5,6,7}
             , {7,6,5,4,3,2,1}
