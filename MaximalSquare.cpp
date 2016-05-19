5a07e63861ff501e1a4c479af3e36667f3b78186
xisisu <xisisu@gmail.com>
2016-05-18 23:50:17 -0400

    Maximal Square

--- MaximalSquare.cpp
+++ MaximalSquare.cpp
@@ -18,14 +18,9 @@
 #include <vector>
 
 class Solution {
-//private:
-//    bool check(int i, int j, int len, std::vector<std::vector<int>> const& matrix) {
-//        if (i+len >= matrix.size())
-//
-//
-//    }
+
 public:
-    int maximalSquare(std::vector<std::vector<char>> const& matrix) {
+    int maximalSquare(std::vector<std::vector<char>> const& matrix) { // 20 ms
         if (matrix.empty() || matrix[0].empty()) { return 0; }
         /*
          * row[i][j] means starting from i,j, count to right, number of successive 1s
@@ -33,45 +28,80 @@ public:
          */
 
         auto M = matrix.size(), N = matrix[0].size();
+        bool existOnes = false;
 
-        // count rows
+        // count rows, also check whether there are '1' in the matrix
         std::vector<std::vector<int>> row(M, std::vector<int>(N, 0));
         for (int i = 0; i < M; ++i) {
-          }
+            int counter = matrix[i][0] == '1' ? 1 : 0;
+            for (int j = 1; j < N; ++j) {
+                if (matrix[i][j] == '1') { ++counter; }
+                else {
+                    while (counter > 0) {
+                        row[i][j-counter] = counter;
+                        --counter;
+                    }
+                    existOnes = true;
+                }
+            }
+            while (counter > 0) {
+                row[i][N-counter] = counter;
+                --counter;
+                existOnes = true;
+            }
+        }
+
+        if (!existOnes) { return 0; }
 
         // count cols
         std::vector<std::vector<int>> col(M, std::vector<int>(N, 0));
-        for (int i = 0; i < N; ++i) {
-            int counter = matrix[0][i] == '1' ? 1 : 0;
-            for (int j = 1; j < M; ++j) {
-                if (matrix[j][i] == '1') { ++counter; }
-
-                if (matrix[j][i] == '0' || j == M-1) {
+        for (int j = 0; j < N; ++j) {
+            int counter = matrix[0][j] == '1' ? 1 : 0;
+            for (int i = 1; i < M; ++i) {
+                if (matrix[i][j] == '1') { ++counter; }
+                else {
                     while (counter > 0) {
-                        row[j-counter][i] = counter;
+                        col[i-counter][j] = counter;
                         --counter;
                     }
                 }
             }
+            while (counter > 0) {
+                col[M-counter][j] = counter;
+                --counter;
+            }
         }
 
-        std::cout << "haha";
+        // now try to find the maximum length, typical dp problem
+        std::vector<std::vector<int>> dp(M, std::vector<int>(N, 0));
+        for (int i = M-1; i >= 0; --i) {
+            for (int j = N-1; j >= 0; --j) {
+                auto val = std::min(row[i][j], col[i][j]);
 
-        return 0;
+                if (i == M-1 || j == N-1) { dp[i][j] = (val == 0) ? 0 : 1; } // at edges, at most can have square 1
+                else if (val == 0) { dp[i][j] = 0; }
+                else { dp[i][j] = std::min(val, dp[i+1][j+1]+1); }
+            }
+        }
+
+        int res = 0;
+        for (auto const& row : dp) {
+            for (auto const& val : row) {
+                res = std::max(res, val);
+            }
+        }
+
+        return res * res;
     }
 };
 
 int main() {
     Solution sol;
     std::vector<std::vector<char>> test = {
-            {'1', '1'}
-            , {'1', '1'}
-//
-//
-//            {'1', '0', '1', '0', '0'}
-//            , {'1', '0', '1', '1', '1'}
-//            , {'1', '1', '1', '1', '1'}
-//            , {'1', '0', '0', '1', '0'}
+            {'1', '0', '1', '0', '0'}
+            , {'1', '0', '1', '1', '1'}
+            , {'1', '1', '1', '1', '1'}
+            , {'1', '0', '0', '1', '0'}
     };
 
     std::cout << sol.maximalSquare(test) << std::endl;
